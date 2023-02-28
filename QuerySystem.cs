f6e7e44864360e96a05905c2d5a4011a69eaf974
Andrey Cherkashin <action.open@gmail.com>
2023-02-28 10:19:43 +0100

    + adding flags to wait for previous/inner jobs to complete

--- QuerySystem.cs
+++ QuerySystem.cs
@@ -34,6 +34,7 @@ namespace Scellecs.Morpeh
 #endif
         }
 
+#if MORPEH_BURST
         /// <summary>
         /// As the world can contain multple jobs, we have to wait until all of them are complete.
         /// By default, we will be using World's JobHandle to wait for all the jobs. In this case, we combine the existing JobHandle
@@ -41,17 +42,41 @@ namespace Scellecs.Morpeh
         /// </summary>
         private void UpdateJobHandles()
         {
-            if (m_jobHandles.length == 0) 
+            if (m_waitUntilAllPreviousJobsCompleted)
+                World.JobHandle.Complete();
+            
+            if (m_queryBuilderJobHandles.length == 0) 
                 return;
             
+            m_jobHandles.Clear();
             var hasWorldJobHandle = !World.JobHandle.IsCompleted;
-            var jobHandlesLength = m_jobHandles.length + (hasWorldJobHandle ? 1 : 0);
-            var jobHandles = new NativeArray<JobHandle>(jobHandlesLength, Allocator.Temp);
+            
+            // collecting unfinished jobs from this system
+            for (var i = 0; i < m_queryBuilderJobHandles.length; i++)
+            {
+                var jobHandle = m_queryBuilderJobHandles.data[i];
+                if (jobHandle.jobHandle.IsCompleted)
+                    continue;
+
+                m_jobHandles.Add(jobHandle.jobHandle);
+            }
+
+            var jobHandles = new NativeArray<JobHandle>(m_jobHandles.length + (hasWorldJobHandle ? 1 : 0), Allocator.Temp);
             for (var i = 0; i < m_jobHandles.length; i++)
-                jobHandles[i] = m_jobHandles.data[i].jobHandle;
+                jobHandles[i] = m_jobHandles.data[i];
+
             if (hasWorldJobHandle)
                 jobHandles[m_jobHandles.length] = World.JobHandle;
-            World.JobHandle = JobHandle.CombineDependencies(jobHandles);
+            
+            var allDependenciesJobHandle = JobHandle.CombineDependencies(jobHandles);
+            
+            // if there is a job that has [WaitUntilInnerJobsCompleted] activated, we should force this system to wait until jobs are completed
+            // otherwise, let the world wait for it in the end of the update (or until some other system wait for it)
+            if (m_waitUntilAllInnerJobsCompleted)
+                allDependenciesJobHandle.Complete();
+            else
+                World.JobHandle = allDependenciesJobHandle;
+            
             jobHandles.Dispose();
         }
 
@@ -65,12 +90,24 @@ namespace Scellecs.Morpeh
             m_executors.Add(newQueryExecutor);
         }
 
-#if MORPEH_BURST
-        private readonly FastList<QueryBuilderJobHandle> m_jobHandles = new();
+        private readonly FastList<QueryBuilderJobHandle> m_queryBuilderJobHandles = new();
+        private readonly FastList<JobHandle> m_jobHandles = new();
+        private bool m_waitUntilAllInnerJobsCompleted;
+        private bool m_waitUntilAllPreviousJobsCompleted;
 
         internal void AddJobHandle(QueryBuilderJobHandle jobHandles)
         {
-            m_jobHandles.Add(jobHandles);
+            m_queryBuilderJobHandles.Add(jobHandles);
+        }
+
+        protected void WaitUntilInnerJobsCompleted(bool enabled = true)
+        {
+            m_waitUntilAllInnerJobsCompleted = enabled;
+        }
+
+        protected void WaitUntilPreviousJobsCompleted(bool enabled = true)
+        {
+            m_waitUntilAllPreviousJobsCompleted = enabled;
         }
 #endif
     }
